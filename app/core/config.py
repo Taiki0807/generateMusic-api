de7c6e30084e090c5fe7ec16fa6da28deb97b5a5
Taiki0807 <hosokawataiki265@gmail.com>
2023-04-09 14:45:47 +0900

    [remove] Remove unnecessary settings

--- app/core/config.py
+++ app/core/config.py
@@ -5,8 +5,6 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI Template"
     API_V1_STR: str = "/api/v1"
 
-    MODEL_PATH: str = "app/ml_model/model.pth"
-
     class Config:
         case_sensitive = True
 
